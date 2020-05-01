07e308e5e98ee6db1363f6e708b44148596911fb
Zach Gilliom <zacharygilliom@gmail.com>
2020-05-01 00:20:44 -0400

    Added the ability to save the files to our specified directory.

--- src/archive.py
+++ src/archive.py
@@ -3,6 +3,7 @@ from O365 import Account
 from O365.message import Message
 from O365.mailbox import MailBox
 import config
+from pathlib import Path
 
 client_secret = config.client_secret
 client_id = config.client_id
@@ -12,9 +13,10 @@ account = Account(credentials)
 
 class email:
 
-    def __init__(self, title, received):
+    def __init__(self, title, received, path):
         self.title = title
         self.received = received
+        self.path = path
 
     def getTitle(self):
         return self.title
@@ -23,6 +25,9 @@ class email:
         date_received = self.received.strftime("%x")
         month_received = self.received.strftime("%B")
         return {'date': date_received, 'month': month_received}
+
+    def getPath(self):
+        return self.path
 
 class emailDestinationDirectory:
 
@@ -46,16 +51,19 @@ class emailDestinationDirectory:
     def createDirectory(self, emailDate):
         if self.checkDirectory(emailDate):
             os.mkdir(os.path.join(self.emailDirectory, emailDate['month']))
+            # TODO: For some reason this errors out now that the April directory is already created, 
             return f"The {emailDate['month']} was created Successfully"
         else:
             print(f"The {emailDate['month']} Directory already exists!")
             return False
 
 
-def getMessages(inbox):
+def getMessages(inbox, folder_path):
     messages = []
     for message in inbox.get_messages():
-        m = email(title=message.subject, received=message.received)
+        # TODO: Need to fix the path where the file is saved.  I think this should be a method in the email class.
+        emailfilepath = message.save_as_eml(to_path=Path(os.path.join(folder_path, f'{message.subject}.eml')))
+        m = email(title=message.subject, received=message.received, path=emailfilepath)
         messages.append(m)
     return messages
 
@@ -78,13 +86,14 @@ if __name__ == '__main__':
     dir_path = '/home/zacharygilliom/Documents/email-folder/'
 
     inbox = mailbox.inbox_folder()
+    
+    inbox_messages = getMessages(inbox=inbox, folder_path=dir_path)
 
-    inbox_messages = getMessages(inbox=inbox)
+    for m in inbox_messages:
+        print(m.getTitle())
+        print(m.getReceived())
+        print(m.getPath())
 
-#    for m in inbox_messages:
-#        print(m.getTitle())
-#        print(m.getReceived())
-#
     destination_directory = emailDestinationDirectory(dir_path)
 
     destination_directory.createDirectory(inbox_messages[0].getReceived())
